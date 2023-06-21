e189c1301de1066914f35554e87720c3c5e4e8cf
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-21 15:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	const firstName = document.getElementsByTagName("input")[0].value;
+	const lastName = document.getElementsByTagName("input")[1].value;
+	alert(firstName +" "+ lastName);
 }
